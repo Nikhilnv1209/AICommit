117f48b9fd9ee5eaad4fd0df124b99cd040bc0a0
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2025-02-04 22:43:35 +0530

    Feat(Sidebar): Implemented the sidebar for the application - updated the globalcss to have new color theme defined - Also added the logo for the project

--- app/(protected)/app-sidebar.tsx
+++ app/(protected)/app-sidebar.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar"
 import { cn } from "@/lib/utils"
-import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react"
+import { Bot, CreditCard, LayoutDashboard, Plus, Presentation } from "lucide-react"
+import Image from "next/image";
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
@@ -28,12 +30,32 @@ const items = [
   }
 ]
 
+const projects = [
+  {
+    name: "Project 1",
+  },
+  {
+    name: "Project 2",
+  },
+  {
+    name: "Project 3",
+  }
+]
+
 const AppSidebar = () => {
   const pathname = usePathname();
+  const { open } = useSidebar();
   return (
     <Sidebar collapsible="icon" variant="floating">
       <SidebarHeader>
-        Logo
+        <div className="flex items-center gap-2">
+          <Image src="/logo.png" width={48} height={48} alt="logo" />
+          {open && (
+            <h1 className="text-xl font-bold text-primary/80">
+              AICommit
+            </h1>)
+          }
+        </div>
       </SidebarHeader>
 
       <SidebarContent>
@@ -59,6 +81,53 @@ const AppSidebar = () => {
                   )
                 })
               }
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            Your projects
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {
+                projects.map((project) => {
+                  return (
+                    <SidebarMenuItem key={project.name}>
+                      <SidebarMenuButton asChild>
+                        <div className="cursor-pointer">
+                          <div className={cn(
+                            "rounded-sm border size-6 flex items-center justify-center text-sm bg-white text-primary",
+                            {
+                              "bg-primary text-white": true
+                            }
+                          )}>
+                            {project.name[0]}
+                          </div>
+                          <span>{project.name}</span>
+                        </div>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  )
+                })
+              }
+
+              {/* create project button */}
+              <div className="h-2"></div>
+              {
+                open && (
+                  <SidebarMenuItem>
+                    <Link href={"/create"}>
+                      <Button variant={"outline"} className="w-fit">
+                        <Plus />
+                        Create Project
+                      </Button>
+                    </Link>
+                  </SidebarMenuItem>
+                )
+              }
+
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
