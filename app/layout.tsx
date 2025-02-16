5befab5f6c7e00e964863ad0f437bd754aba4ed1
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2025-02-16 17:16:45 +0530

    Feat(React-Query): Added react query in the project for efficient data fetching - Created the client and QueryProvider as the client components to wrap the whole app following nextjs norms

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import QueryProvider from "@/lib/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +31,10 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Toaster position="top-center" expand theme="system"/>
-          {children}
+          <Toaster position="top-center" expand theme="system" />
+          <QueryProvider>
+            {children}
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>
