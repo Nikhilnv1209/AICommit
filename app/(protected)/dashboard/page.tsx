654ad1f4334ab135dc38fe18f0596c28506eedcb
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2025-02-17 23:49:56 +0530

    Update(DashBoard): Updated the dashboard to have the GithubUrl button - Added the placeholders for team-members, invite, archive, ai questions, meetings and commit history

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,15 +1,50 @@
 "use client";
-import { useUser } from '@clerk/nextjs'
+import useProject from '@/hooks/use-project';
+import { ExternalLink, ExternalLinkIcon, Github, GithubIcon } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
 const DashBoard = () => {
-  const { user } = useUser();
+  const { project } = useProject();
 
   return (
-    <>
-    <div>{user?.fullName}</div>
-    <div>{user?.lastName}</div>
-    </>
+    <div>
+      <div className='flex items-center justify-between flex-wrap gap-y-4'>
+        {/* github link */}
+        <div className='w-fit rounded-md bg-primary px-4 py-3'>
+          <div className="flex items-center">
+            <Github className='text-white size-5' />
+            <div className="ml-2">
+              <p className='text-sm font-medium text-white'>
+                This Project is linked to {' '}
+                <Link href={project?.githubUrl || ''} className='inline-flex items-center text-white/80 hover:underline'>
+                  {project?.githubUrl}
+                  <ExternalLinkIcon className='ml-1 size-4' />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-4"></div>
+
+        <div className='flex items-center gap-4'>
+            TeamsMembers
+            Addmembers
+            ArchiveButton         
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-5'>
+          AskQuestionCard
+          Meeting card
+        </div>
+      </div>
+
+      <div className="mt-8"></div>
+      CommitLogs
+    </div>
   )
 }
 
