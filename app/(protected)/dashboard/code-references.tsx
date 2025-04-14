f86faeb65b610e714369c235490d7f873da17af0
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2025-04-14 22:50:23 +0530

    Fix(File selection from file references): Updated the code to select the first file name when the file references are loaded at first time in the tab

--- app/(protected)/dashboard/code-references.tsx
+++ app/(protected)/dashboard/code-references.tsx
@@ -3,47 +3,60 @@ import { Tabs, TabsContent } from '@/components/ui/tabs';
 import { cn } from '@/lib/utils';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { lucario } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import {useState} from 'react'
+import { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
 
 type Props = {
   fileReferences: { fileName: string, sourceCode: string, summary: string }[];
-}
+};
 
 const CodeReferences = ({ fileReferences }: Props) => {
-  const [tab, setTab] = useState(fileReferences[0]?.fileName);
+  const [tab, setTab] = useState<string | undefined>(undefined);
+
+  // Automatically select the first file reference when fileReferences changes
+  useEffect(() => {
+    if (fileReferences.length > 0 && tab !== fileReferences[0].fileName) {
+      setTab(fileReferences[0].fileName);
+    }
+  }, [fileReferences]);
 
   if (fileReferences.length === 0) return null;
-  
+
   return (
-    <div className='max-w-[70vw]'>
+    <div className="max-w-[70vw] mt-2">
       <Tabs value={tab} onValueChange={setTab}>
-        <div className='overflow-auto flex gap-2 bg-gray-200 p-2 rounded-md'> 
-          {
-            fileReferences.map((file) => ( 
-                <button key={file.fileName} 
-                onClick={() => setTab(file.fileName)} className={cn(
-                  'px-3 py-1.5 text-sm font-medium rounded-md transition-colors whitespace-nowrap text-muted-foreground hover:bg-muted',
-                  {
-                    "bg-primary text-primary-foreground" : tab === file.fileName,
-                  }
-                  )}>
-                  {file.fileName}
-                </button>
-              )
-            )
-          }
+        <div className="overflow-auto flex gap-2 bg-gray-200 p-1.5 rounded-md">
+          {fileReferences.map((file) => (
+            <Button
+              key={file.fileName}
+              onClick={() => setTab(file.fileName)}
+              variant={'ghost'}
+              size="sm"
+              className={cn(
+                'px-3 py-1.5 text-sm font-medium rounded-md transition-colors whitespace-nowrap text-muted-foreground bg-muted hover:bg-muted-foreground hover:text-primary-foreground',
+                {
+                  'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground': tab === file.fileName,
+                }
+              )}
+            >
+              {file.fileName}
+            </Button>
+          ))}
         </div>
-        { fileReferences.map(file => (
-            <TabsContent key={file.fileName} value={file.fileName} className='max-h-[40vh] overflow-y-auto max-w-7xl rounded-md'>
-              <SyntaxHighlighter language='typescript' style={lucario}>
-                {file.sourceCode}
-              </SyntaxHighlighter>
-            </TabsContent>  
-          )
-        )}
+        {fileReferences.map((file) => (
+          <TabsContent
+            key={file.fileName}
+            value={file.fileName}
+            className="max-h-[40vh] overflow-y-auto max-w-7xl rounded-md custom-markdown-scroll"
+          >
+            <SyntaxHighlighter language="typescript" style={lucario} wrapLines={true} wrapLongLines={true}>
+              {file.sourceCode}
+            </SyntaxHighlighter>
+          </TabsContent>
+        ))}
       </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default CodeReferences
+export default CodeReferences;
